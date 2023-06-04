1b57f06f7434d3159d0c7e696e78d7fb51f8ea57
Daniel Roe <daniel@roe.dev>
2023-06-04 23:00:50 +0100

    style: lint

--- src/server/utils/session.ts
+++ src/server/utils/session.ts
@@ -1,11 +1,11 @@
 import { H3Event } from 'h3'
 
-export function getUserSession (event: H3Event) {
+export function getUserSession(event: H3Event) {
   const config = useRuntimeConfig()
   return useSession(event, { password: config.sessionPassword })
 }
 
-export function setUserSession (event : H3Event, data: any) {
+export function setUserSession(event: H3Event, data: any) {
   const config = useRuntimeConfig()
   return updateSession(event, { password: config.sessionPassword }, data)
 }

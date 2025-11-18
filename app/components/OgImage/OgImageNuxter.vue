674bb0cd89fceb8f670285ce2e3ac1ffd0c8af9e
Valentin Chmara <valentinchmara@gmail.com>
2025-11-18 11:00:11 +0100

    fix: og image src

--- app/components/OgImage/OgImageNuxter.vue
+++ app/components/OgImage/OgImageNuxter.vue
@@ -15,7 +15,7 @@ const { format } = Intl.NumberFormat('en-GB', {})
     <div class="flex flex-col w-1/3 items-center py-10 justify-between text-white">
       <div class="flex flex-col items-center">
         <UAvatar
-          :src="`https://avatars.githubusercontent.com/u/${contributor.githubId}`"
+          :src="`u/${contributor.githubId}?v=4&s=32`"
           :ui="{ root: 'flex size-32 text-5xl', image: 'rounded-full' }"
         />
         <div class="text-3xl pt-[18px]">

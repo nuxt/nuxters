9dff300a4d5cd2f9694c422084f38859f860e8ac
Sébastien Chopin <atinux@gmail.com>
2026-04-28 10:36:45 +0200

    chore: check if get is not a function instead

--- server/plugins/compat.ts
+++ server/plugins/compat.ts
@@ -2,9 +2,9 @@ import { getRequestHeader } from 'h3'
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('request', async (event) => {
-    if (typeof event.req.headers.get === 'undefined') {
-      // @ts-expect-error support newer version of h3
-      event.req.headers.get = getRequestHeader.bind(null, event)
+    if (typeof event.req.headers.get !== 'function') {
+      // @ts-expect-error cross-version compat between h3 request header types
+      event.req.headers.get = (name: string) => getRequestHeader(event, name) ?? null
     }
   })
 })

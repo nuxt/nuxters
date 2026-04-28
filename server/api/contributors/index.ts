f3dc43cbc655eefa8d84cd0c17a83a4b999fe1b5
Sébastien Chopin <atinux@gmail.com>
2026-04-28 15:42:29 +0200

    fix: use env variable for origin

--- server/api/contributors/index.ts
+++ server/api/contributors/index.ts
@@ -1,4 +1,5 @@
 export default eventHandler(async (event) => {
   const url = getRequestURL(event)
-  return fetch(`${url.origin}/contributors.json`).then(res => res.json())
+  const origin = process.env.NUXT_URL || process.env.VERCEL_PROJECT_PRODUCTION_URL || url.origin
+  return fetch(`${origin}/contributors.json`).then(res => res.json())
 })

2b7bfb131ccf24eb5ac286e9fc0b51afed5581ad
Sébastien Chopin <atinux@gmail.com>
2026-04-28 15:51:20 +0200

    fix: compat version with old og image

--- server/middleware/card.ts
+++ server/middleware/card.ts
@@ -5,4 +5,8 @@ export default defineEventHandler(async (event) => {
     const [,,username] = event.path.split('/')
     return sendRedirect(event, `/_og/r/${username}.png`)
   }
+  if (event.method === 'GET' && event.path.startsWith('/__og-image__/image/') && event.path.endsWith('/og.png')) {
+    const [,,,username] = event.path.split('/')
+    return sendRedirect(event, `/_og/r/${username}.png`)
+  }
 })

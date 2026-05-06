9dc24ca993ae8609067fc39673a3c087f4fddf3f
Benjamin Canac <canacb1@gmail.com>
2026-05-06 10:41:15 +0200

    chore(search): clean contributors api routes

--- server/api/contributors/[username].ts
+++ server/api/contributors/[username].ts
@@ -1,22 +1,22 @@
-export default eventHandler(async (event) => {
+export default defineEventHandler(async (event) => {
   const username = getRouterParam(event, 'username')
-
   if (!username) {
     throw createError({
       statusCode: 400,
       statusMessage: 'Username missing',
     })
   }
+
   const contributors = await fetchContributors(event)
 
   const index = contributors?.findIndex(contributor => contributor.username.toLowerCase() === username.toLocaleLowerCase()) ?? -1
-
   if (index === -1) {
     throw createError({
       statusCode: 404,
       statusMessage: 'Contributor not found',
     })
   }
+
   return {
     ...contributors![index]!,
     rank: index + 1,
--- server/api/contributors/index.ts
+++ server/api/contributors/index.ts
@@ -1,5 +1,3 @@
-export default eventHandler(async (event) => {
-  const url = getRequestURL(event)
-  const origin = process.env.NUXT_URL || process.env.VERCEL_PROJECT_PRODUCTION_URL || url.origin
-  return fetch(`${origin}/contributors.json`).then(res => res.json())
+export default defineEventHandler(async (event) => {
+  return fetchContributors(event)
 })
